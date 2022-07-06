886529a612d8385aeb2c7363a5c2687de7e75322
patichenko <patichenko@gmail.com>
2022-07-06 18:57:04 +0300

    Add files via upload

---
+++ page-switching.js
@@ -0,0 +1,32 @@
+button1.onclick = function() {
+    		document.getElementById("form__pg1").className = "form__pg__off";
+    		document.getElementById("form__pg2").className = "form__pg__show";
+  		};
+  		button2.onclick = function() {
+    		document.getElementById("form__pg1").className = "form__pg__show";
+    		document.getElementById("form__pg2").className = "form__pg__off";
+  		};
+        button3.onclick = function() {
+    		document.getElementById("form__pg3").className = "form__pg__show";
+    		document.getElementById("form__pg2").className = "form__pg__off";
+  		};
+        button4.onclick = function() {
+    		document.getElementById("form__pg2").className = "form__pg__show";
+    		document.getElementById("form__pg3").className = "form__pg__off";
+  		};
+        button5.onclick = function() {
+    		document.getElementById("form__pg4").className = "form__pg__show";
+    		document.getElementById("form__pg3").className = "form__pg__off";
+  		};
+        button6.onclick = function() {
+    		document.getElementById("form__pg3").className = "form__pg__show";
+    		document.getElementById("form__pg4").className = "form__pg__off";
+  		};
+        button7.onclick = function() {
+    		document.getElementById("form__pg5").className = "form__pg__show";
+    		document.getElementById("form__pg4").className = "form__pg__off";
+  		};
+        button8.onclick = function() {
+    		document.getElementById("form__pg4").className = "form__pg__show";
+    		document.getElementById("form__pg5").className = "form__pg__off";
+  		};
